bcc104efedbbd6747872ffcee97fcb73d8f58fde
izmipo <2fromage2@gmail.com>
2017-10-06 15:39:45 +0900

    again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,11 +63,12 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'glacial-fjord-16940.herokuapp.com' }
+  host = 'glacial-fjord-16940.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
-    :authentication => :plain,glacial-fjord-16940
+    :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
